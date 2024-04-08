5669d1510f406a5d50b7a282bbbab81c66c11198
SyntaxTheDev <138131313+SyntaxTheDev@users.noreply.github.com>
2024-04-08 10:28:16 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -42,6 +42,9 @@ if game.PlaceId == 1204176775 then
     local WeaponsTab = Window:CreateTab("Weapons", 4483362458)
     local WeaponsSection = WeaponsTab:CreateSection("Weapon Options")
 
+    local VisualsTab = Window:CreateTab("Visuals", 4483362458)
+    local VisualsSection = VisualsTab:CreateSection("Visual Options")
+
     local MiscTab = Window:CreateTab("Misc", 4483362458)
     local MiscSection = MiscTab:CreateSection("Misc")
 
@@ -60,6 +63,8 @@ if game.PlaceId == 1204176775 then
         Actions = {}, -- Notification Buttons
      })
 
+     -- Main Tab
+
      local Button = MainTab:CreateButton({
         Name = "Infinite Jump",
         Callback = function()
@@ -69,6 +74,190 @@ if game.PlaceId == 1204176775 then
                     game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
                 end
             end)
+        end,
+     })
+
+     -- Visuals Tab
+
+     local ESP = VisualsTab:CreateToggle({
+        Name = "Enable ESP",
+        CurrentValue = false,
+        Flag = "ESP", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+        Callback = function()
+        end,
+     })
+
+     local NameESP = VisualsTab:CreateToggle({
+        Name = "Names",
+        CurrentValue = false,
+        Flag = "NameESP", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+        Callback = function(Value)
+
+            local function API_Check()
+                if Drawing == nil then
+                    return "No"
+                else
+                    return "Yes"
+                end
+            end
+            
+            local Find_Required = API_Check()
+            
+            if Find_Required == "No" then
+                game:GetService("StarterGui"):SetCore("SendNotification",{
+                    Title = "Exunys Developer";
+                    Text = "ESP script could not be loaded because your exploit is unsupported.";
+                    Duration = math.huge;
+                    Button1 = "OK"
+                })
+
+                return
+            end
+            
+            local Players = game:GetService("Players")
+            local RunService = game:GetService("RunService")
+            local UserInputService = game:GetService("UserInputService")
+            local Camera = workspace.CurrentCamera
+            
+            local Typing = false
+            
+            _G.SendNotifications = false   -- If set to true then the script would notify you frequently on any changes applied and when loaded / errored. (If a game can detect this, it is recommended to set it to false)
+            _G.DefaultSettings = false   -- If set to true then the ESP script would run with default settings regardless of any changes you made.
+            
+            _G.TeamCheck = false   -- If set to true then the script would create ESP only for the enemy team members.
+            
+            _G.ESPVisible = true   -- If set to true then the ESP will be visible and vice versa.
+            _G.TextColor = Color3.fromRGB(255, 255, 255)   -- The color that the boxes would appear as.
+            _G.TextSize = 12   -- The size of the text.
+            _G.Center = true   -- If set to true then the script would be located at the center of the label.
+            _G.Outline = true   -- If set to true then the text would have an outline.
+            _G.OutlineColor = Color3.fromRGB(255, 255, 255)   -- The outline color of the text.
+            _G.TextTransparency = 0.7   -- The transparency of the text.
+            _G.TextFont = Drawing.Fonts.UI   -- The font of the text. (UI, System, Plex, Monospace) 
+            
+            _G.DisableKey = Enum.KeyCode.Q   -- The key that disables / enables the ESP.
+            
+            local function CreateESP()
+                for _, v in next, Players:GetPlayers() do
+                    if v.Name ~= Players.LocalPlayer.Name then
+                        local ESP = Drawing.new("Text")
+            
+                        RunService.RenderStepped:Connect(function()
+                            if workspace:FindFirstChild(v.Name) ~= nil and workspace[v.Name]:FindFirstChild("HumanoidRootPart") ~= nil then
+                                local Vector, OnScreen = Camera:WorldToViewportPoint(workspace[v.Name]:WaitForChild("Head", math.huge).Position)
+            
+                                ESP.Size = _G.TextSize
+                                ESP.Center = _G.Center
+                                ESP.Outline = _G.Outline
+                                ESP.OutlineColor = _G.OutlineColor
+                                ESP.Color = _G.TextColor
+                                ESP.Transparency = _G.TextTransparency
+                                ESP.Font = _G.TextFont
+            
+                                if OnScreen == true then
+                                    local Part1 = workspace:WaitForChild(v.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position
+                                    local Part2 = workspace:WaitForChild(Players.LocalPlayer.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position or 0
+                                    local Dist = (Part1 - Part2).Magnitude
+                                    ESP.Position = Vector2.new(Vector.X, Vector.Y - 25)
+                                    ESP.Text = ("("..tostring(math.floor(tonumber(Dist)))..") "..v.Name.." ["..workspace[v.Name].Humanoid.Health.."]")
+                                    if _G.TeamCheck == true then 
+                                        if Players.LocalPlayer.Team ~= v.Team then
+                                            ESP.Visible = _G.ESPVisible
+                                        else
+                                            ESP.Visible = false
+                                        end
+                                    else
+                                        ESP.Visible = _G.ESPVisible
+                                    end
+                                else
+                                    ESP.Visible = false
+                                end
+                            else
+                                ESP.Visible = false
+                            end
+                        end)
+            
+                        Players.PlayerRemoving:Connect(function()
+                            ESP.Visible = false
+                        end)
+                    end
+                end
+            
+                Players.PlayerAdded:Connect(function(Player)
+                    Player.CharacterAdded:Connect(function(v)
+                        if v.Name ~= Players.LocalPlayer.Name then 
+                            local ESP = Drawing.new("Text")
+                
+                            RunService.RenderStepped:Connect(function()
+                                if workspace:FindFirstChild(v.Name) ~= nil and workspace[v.Name]:FindFirstChild("HumanoidRootPart") ~= nil then
+                                    local Vector, OnScreen = Camera:WorldToViewportPoint(workspace[v.Name]:WaitForChild("Head", math.huge).Position)
+                
+                                    ESP.Size = _G.TextSize
+                                    ESP.Center = _G.Center
+                                    ESP.Outline = _G.Outline
+                                    ESP.OutlineColor = _G.OutlineColor
+                                    ESP.Color = _G.TextColor
+                                    ESP.Transparency = _G.TextTransparency
+                
+                                    if OnScreen == true then
+                                        local Part1 = workspace:WaitForChild(v.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position
+                                    local Part2 = workspace:WaitForChild(Players.LocalPlayer.Name, math.huge):WaitForChild("HumanoidRootPart", math.huge).Position or 0
+                                        local Dist = (Part1 - Part2).Magnitude
+                                        ESP.Position = Vector2.new(Vector.X, Vector.Y - 25)
+                                        ESP.Text = ("("..tostring(math.floor(tonumber(Dist)))..") "..v.Name.." ["..workspace[v.Name].Humanoid.Health.."]")
+                                        if _G.TeamCheck == true then 
+                                            if Players.LocalPlayer.Team ~= Player.Team then
+                                                ESP.Visible = _G.ESPVisible
+                                            else
+                                                ESP.Visible = false
+                                            end
+                                        else
+                                            ESP.Visible = _G.ESPVisible
+                                        end
+                                    else
+                                        ESP.Visible = false
+                                    end
+                                else
+                                    ESP.Visible = false
+                                end
+                            end)
+                
+                            Players.PlayerRemoving:Connect(function()
+                                ESP.Visible = false
+                            end)
+                        end
+                    end)
+                end)
+            end
+            
+            if _G.DefaultSettings == true then
+                _G.TeamCheck = false
+                _G.ESPVisible = false
+                _G.TextColor = Color3.fromRGB(40, 90, 255)
+                _G.TextSize = 14
+                _G.Center = true
+                _G.Outline = false
+                _G.OutlineColor = Color3.fromRGB(0, 0, 0)
+                _G.DisableKey = Enum.KeyCode.Q
+                _G.TextTransparency = 0.75
+            end
+            
+            UserInputService.TextBoxFocused:Connect(function()
+                Typing = true
+            end)
+            
+            UserInputService.TextBoxFocusReleased:Connect(function()
+                Typing = false
+            end)
+            
+            local Success, Errored = pcall(function()
+                CreateESP()
+            end)
+
+            if Value then
+                _G.ESPVisible = not _G.ESPVisible
+            end
+            
         end,
      })
 
